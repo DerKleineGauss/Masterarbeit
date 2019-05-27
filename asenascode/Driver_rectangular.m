dfc057437e899b47b8de66f06fd157c15bc2ae75
Matthias Jaeger <Matthias.Jaeger@tu-dortmund.de>
2019-05-27 14:06:55 +0200

    basics working commit

--- asenascode/Driver_rectangular.m
+++ asenascode/Driver_rectangular.m
@@ -1,11 +1,12 @@
 setLatex;
 close, clear()
 clc
+opengl('save', 'software')
 
 tic
 % Driver script for solving the 2D Poisson equation
 params = GlobalParams;
-params.testing = true;
+params.testing = false;
 
 %% Umskalierung
 epsilon= params.constants.e / params.constants.hbar;
@@ -16,8 +17,8 @@ gamma= sqrt(epsilon*2*params.constants.m/params.constants.hbar);
 params.Nx = 3;
 params.Ny = 3;
 params.Nfaces = 4;
-params.Kx= 20;
-params.Ky= 20;
+params.Kx= 50;
+params.Ky= 50;
 % derived values
 params.Npx = params.Nx + 1; params.Npy = params.Ny + 1; params.Nfp = params.Npx;
 params.Np = params.Npx * params.Npy;
@@ -33,10 +34,12 @@ params = StartUp2D_rectangular(params);
 params.p_DFT = reshape(params.p_DFT, params.Np, params.K);
 params.P_DFT = params.p_DFT(params.Fmask(:),:);    % face p's
 % adapt boundarys
-[params]=adaptBoundaryMap(params);
+% [params]=adaptBoundaryMap(params);
+[params] = BuildBCMaps2D(params);
+
 if(params.testing)
     figure(10)
-    plot_sorted(params.x(:), params.p_DFT(:))
+    plot(params.x(:), params.p_DFT(:), 'x')
 end
 
 B= functionB(params);
@@ -53,18 +56,20 @@ end
 [a, b, c] = params.get_abc(gamma, epsilon);
 
 % old bc's in space regime
-% bed = Lr/2-abs(Fx(mapD)) < NODETOL;
-% fh= @(k) a.*cos(Fy(mapD(bed))*k).*log(1+exp(-b.*k^2+c));
-% f= integral(fh,-2*c,2*c,'ArrayValued', true);
-%
-% figure(101)
-% plot_sorted(Fy(mapD(bed)),f)
-%
-% uD= zeros(Nfp*Nfaces, K);
-% uD(mapD(bed))= f;
+bed = params.Lr_scaled/2-abs(params.Fx(params.mapD)) < params.NODETOL;
+fh= @(k) a.*cos(params.Fy(params.mapD(bed))*k).*log(1+exp(-b.*k^2+c));
+f= integral(fh,-2*c,2*c,'ArrayValued', true);
+
+if (params.testing)
+    figure(101)
+    plot_sorted(params.Fy(params.mapD(bed)),f)
+end
+
+uD= zeros(params.Nfp*params.Nfaces, params.K);
+uD(params.mapD(bed))= f;
 
 % new bc's in k regime
-uD = rhs_dft(params, a,b,c, Phi);
+% uD = rhs_dft(params, a,b,c, Phi);
 
 % set up Neumann boundary conditions
 qN = zeros(params.Nfp*params.Nfaces, params.K);
@@ -78,43 +83,44 @@ rhs= 0;
 rhs = -params.MassMatrix*(params.J.*rhs) + Aqbc;
 
 %% solve system
-u_hat = (Phi*A*Phi')\rhs(:);
-u = Phi' * u_hat;
+% u_hat = (Phi*A*Phi')\(Phi*rhs(:));
+% u = Phi' * u_hat;
+u = A\rhs(:);
 u = reshape(u, params.Np, params.K);
 
-% figure(1)
-% %hack
-% %xi = sqrt(hbar^2 / m / (a0*e));
-% x = params.x; y = params.y;
-% x = x/gamma*2*1e9;
-% y = y/gamma*2*1e9;
-% u = u*2;
-% %
-% x1= x(:);
-% x2= y(:);
-% m1= min(x1);
-% m2= max(x1);
-% dx= (m2-m1)/400;
-% xlin= m1:dx:m2;
-% m1= min(x2);
-% m2= max(x2);
-% dy= (m2-m1)/400;
-% ylin= m1:dy:m2;
-% [X, Y]= meshgrid(xlin, ylin);
-% f= scatteredInterpolant(x(:), y(:),real(u(:)),'linear');
-% %f= scatteredInterpolant(x(:), y(:),imag(u(:)),'linear');
-% Z= f(X,Y);
-% mesh(X,Y, f(X,Y))
-% xlabel('$r / \xi$')
-% ylabel('$q / \xi$')
-% zlabel('$\rho(r,q)$')
-% 
-% figure(2)
-% rho_L= Z(abs(Y) <= params.NODETOL);
-% xl= X(abs(Y) <= params.NODETOL);
-% plot(xl,rho_L)
-% xlabel('$x / \xi$')
-% ylabel('$n(x)$')
+figure(1)
+%hack
+%xi = sqrt(hbar^2 / m / (a0*e));
+x = params.x; y = params.y;
+x = x/gamma*2*1e9;
+y = y/gamma*2*1e9;
+u = u*2;
+%
+x1= x(:);
+x2= y(:);
+m1= min(x1);
+m2= max(x1);
+dx= (m2-m1)/400;
+xlin= m1:dx:m2;
+m1= min(x2);
+m2= max(x2);
+dy= (m2-m1)/400;
+ylin= m1:dy:m2;
+[X, Y]= meshgrid(xlin, ylin);
+f= scatteredInterpolant(x(:), y(:),real(u(:)),'linear');
+%f= scatteredInterpolant(x(:), y(:),imag(u(:)),'linear');
+Z= f(X,Y);
+mesh(X,Y, f(X,Y))
+xlabel('$r / \xi$')
+ylabel('$q / \xi$')
+zlabel('$\rho(r,q)$')
+
+figure(2)
+rho_L= Z(abs(Y) <= params.NODETOL);
+xl= X(abs(Y) <= params.NODETOL);
+plot(xl,rho_L)
+xlabel('$x / \xi$')
+ylabel('$n(x)$')
 
 %eigenwerte = eigs(A)
 
