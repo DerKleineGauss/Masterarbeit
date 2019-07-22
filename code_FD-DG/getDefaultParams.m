5b1697295c163ced5b7c2282ac1a479e9750993e
Matthias Jäger <jaeger@hft.e-technik.uni-dortmund.de>
2019-07-22 17:23:06 +0200

    better default params

--- code_FD-DG/getDefaultParams.m
+++ code_FD-DG/getDefaultParams.m
@@ -4,27 +4,27 @@ params = GlobalParams;
 
 % result and plot parameters
 params.movieName = 'results/Tfinal_15000_N3_K220.avi';
-params.makeMovie = true;
+params.makeMovie = false;
 params.plot_logarithmic = true;
 params.saveResults = false;
 
 % Order of polymomials used for approximation (x direction)
 params.N = 1;
 % Number of cells for DG discretization (x direction)
-params.K = 100;
+params.K = 140;
 % Number of cells (y direction)
-params.Ny = 100;
+params.Ny = 160;
 % Number of interfaces (y direction)
 params.Npy = params.Ny+  1;
 % Voltage
-params.U = +0.0;
+params.U = -0.25;
 params.rampTime = 0.2;
 % final time
 params.FinalTime = 150;
 % time stepping mode ( one of rk4 , rk2ssp , )
 params.mode = 'rk2ssp';
 % do timestepping?
-params.timestepping = true;
+params.timestepping = false;
 
 if (params.K * params.Ny * (params.N+1) > 150)
     params.testing = false;
@@ -33,7 +33,7 @@ else
 end
 
 % BC related params
-params.withCAP = false;
+params.withCAP = true;
 params.constants.mu_l = newtonRaphson(@nullstellenSucheMu, 1.5*params.constants.e, params);
 % params.constants.E_c = -params.U*params.constants.e;
 params.constants.mu_r = newtonRaphson(@nullstellenSucheMu, 1.5*params.constants.e, params);
