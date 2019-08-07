c92d3b8f504b54a370216c8be2495a1077a8cd73
Matthias Jäger <jaeger@hft.e-technik.uni-dortmund.de>
2019-08-07 14:54:36 +0200

    adjust integral boundary

--- code_FD-DG/flux_rhs.m
+++ code_FD-DG/flux_rhs.m
@@ -16,24 +16,24 @@ function [rhs] = flux_rhs(params, D_l, D_r, R, eigs_A)
     map_right_v = Lr/2 - x < NODETOL;
     if (params.N == 0)
         map_left_v  = find(Lr/2 + (x-params.hx/2) < NODETOL);
-        map_right_v = find(Lr/2 - (x+params.hx/2) < NODETOL);        
+        map_right_v = find(Lr/2 - (x+params.hx/2) < NODETOL);
     end
     fh_left = @(k) a.*cos(y(map_left_v )*k).*log(1+exp(-b.*k^2+c_l));
     fh_right= @(k) a.*cos(y(map_right_v)*k).*log(1+exp(-b.*k^2+c_r));
-    
+
     f = zeros(systemsize, 1);
-    f(map_left_v)  = integral(fh_left,  -2*c_l, 2*c_l,'ArrayValued', true);
-    f(map_right_v) = integral(fh_right, -2*c_r, 2*c_r,'ArrayValued', true);
+    f(map_left_v)  = integral(fh_left,  -2*sqrt(c_l/b), 2*sqrt(c_l/b),'ArrayValued', true);
+    f(map_right_v) = integral(fh_right, -2*sqrt(c_r/b), 2*sqrt(c_r/b),'ArrayValued', true);
     f = reshape(f, Np*K, Ny);
-    
+
     map_pos = find(eigs_A>0);
     map_neg = find(eigs_A<0);
-    
+
     RT_pos = 0*RT; RT_neg = 0*RT;
 %     RT_pos(:, map_pos) = RT(:, map_pos);
 %     RT_neg(:, map_neg) = RT(:, map_neg);
     RT_pos( map_pos, :) = RT( map_pos, :);
     RT_neg( map_neg, :) = RT( map_neg, :);
     rhs = D_l*strangeMatrixMultiplication(RT_pos , f) + D_r*strangeMatrixMultiplication(RT_neg , f);
-    
+
 end
