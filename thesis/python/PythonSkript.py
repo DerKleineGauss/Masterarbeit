c3975dfd9d3023ed930c6b97a622c454cace9597
Matthias Jaeger <matthias.jaeger@tu-dortmund.de>
2020-01-09 15:35:11 +0100

    up from laptop

--- thesis/python/PythonSkript.py
+++ thesis/python/PythonSkript.py
@@ -1,7 +1,7 @@
 ##################################################### Import system libraries ######################################################
 import matplotlib as mpl
-mpl.rcdefaults()
-mpl.rcParams.update(mpl.rc_params_from_file('python/meine-matplotlibrc'))
+# mpl.rcdefaults()
+# mpl.rcParams.update(mpl.rc_params_from_file('python/meine-matplotlibrc'))
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.constants as const
@@ -48,7 +48,7 @@ def plot_test(file_in, file_out, file_r_out, realteil):
         temp = rate[map]
         r[i] = round(np.mean(temp[1:-1]), 2)
         s[i] = round(np.std(temp[1:-1]), 2)
-        if not(i==b-1):
+        if not(i == b-1):
             f.write(str(r[i])+'\pm'+str(s[i]) + ', ')
         else:
             f.write(str(r[i])+'\pm'+str(s[i]))
@@ -71,22 +71,35 @@ def plot_test(file_in, file_out, file_r_out, realteil):
     plt.savefig(file_out)
 
 
-file_out = '../plots/test1_r.pdf'
-file_in = 'data/test1_real.txt'
-file_r_out = 'test1_r_r.tex'
-plot_test(file_in, file_out, file_r_out, True)
-file_out = '../plots/test1_i.pdf'
-file_in = 'data/test1_imag.txt'
-file_r_out = 'test1_r_i.tex'
-plot_test(file_in, file_out, file_r_out, False)
-file_rel_out = '../plots/test1_r_rel.pdf'
-file_rel_in = 'data/test1_rel_real.txt'
-file_r_rel_out = 'test1_r_r_rel.tex'
-plot_test(file_rel_in, file_rel_out, file_r_rel_out, True)
-file_rel_out = '../plots/test1_i_rel.pdf'
-file_rel_in = 'data/test1_rel_imag.txt'
-file_r_rel_out = 'test1_r_i_rel.tex'
-plot_test(file_rel_in, file_rel_out, file_r_rel_out, False)
+# file_out = '../plots/test1_r.pdf'
+# file_in = 'data/test1_real.txt'
+# file_r_out = 'test1_r_r.tex'
+# plot_test(file_in, file_out, file_r_out, True)
+# file_out = '../plots/test1_i.pdf'
+# file_in = 'data/test1_imag.txt'
+# file_r_out = 'test1_r_i.tex'
+# plot_test(file_in, file_out, file_r_out, False)
+# file_rel_out = '../plots/test1_r_rel.pdf'
+# file_rel_in = 'data/test1_rel_real.txt'
+# file_r_rel_out = 'test1_r_r_rel.tex'
+# plot_test(file_rel_in, file_rel_out, file_r_rel_out, True)
+# file_rel_out = '../plots/test1_i_rel.pdf'
+# file_rel_in = 'data/test1_rel_imag.txt'
+# file_r_rel_out = 'test1_r_i_rel.tex'
+# plot_test(file_rel_in, file_rel_out, file_r_rel_out, False)
+
+from scipy.io import loadmat
+annots = loadmat('data/test6Results_Lr66.mat')
+x = annots['test6Results'][0][0][0]
+V = annots['test6Results'][0][0][1]
+I_over_x = annots['test6Results'][0][0][2]
+I = annots['test6Results'][0][0][3]
+plt.plot(V, I)
+# plt.savefig('out.pdf')
+print(V)
+print(I)
+plt.show()
+
 
 ################################ FREQUENTLY USED CODE ################################
 #
